04deffe14229fe44a77408233fce2c99bf01a336
lbdrkfcc <lbdrkfcc@yandex.ru>
2017-03-26 15:00:36 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,14 +1,101 @@
 $(function() {
 
 	//E-mail Ajax Send
-	$('form').submit(function(event){ //Change
+	$('#order-wood').submit(function(event){ //Change
         event.preventDefault();
         $.ajax({
-          url: "mail.php", //Change
+          url: "mail-wood.php", //Change
           type: "POST",
-          data: $('form').serialize(), //Change
+          data: $('#order-wood').serialize(), //Change
           success: function(response) {
             alert("Спасибо за информацию!"); //Change
+            $.magnificPopup.close();
+            $('input[type="text"]').val('');
+          },
+          error: function(response) {
+            alert("Ошибка при отправке сообщения"); //Change
+         }
+        });
+    });
+    
+    $('#order-metall').submit(function(event){ //Change
+        event.preventDefault();
+        $.ajax({
+          url: "mail-metall.php", //Change
+          type: "POST",
+          data: $('#order-metall').serialize(), //Change
+          success: function(response) {
+            alert("Спасибо за информацию!"); //Change
+            $.magnificPopup.close();
+            $('input[type="text"]').val('');
+          },
+          error: function(response) {
+            alert("Ошибка при отправке сообщения"); //Change
+         }
+        });
+    });
+    
+    $('#order-rabica').submit(function(event){ //Change
+        event.preventDefault();
+        $.ajax({
+          url: "mail-rabica.php", //Change
+          type: "POST",
+          data: $('#order-rabica').serialize(), //Change
+          success: function(response) {
+            alert("Спасибо за информацию!"); //Change
+            $.magnificPopup.close();
+            $('input[type="text"]').val('');
+          },
+          error: function(response) {
+            alert("Ошибка при отправке сообщения"); //Change
+         }
+        });
+    });
+    
+    $('#callback').submit(function(event){ //Change
+        event.preventDefault();
+        $.ajax({
+          url: "mail-callback.php", //Change
+          type: "POST",
+          data: $('#callback').serialize(), //Change
+          success: function(response) {
+            alert("Спасибо за информацию!"); //Change
+            $.magnificPopup.close();
+            $('input[type="text"]').val('');
+          },
+          error: function(response) {
+            alert("Ошибка при отправке сообщения"); //Change
+         }
+        });
+    });
+    
+    $('#questions').submit(function(event){ //Change
+        event.preventDefault();
+        $.ajax({
+          url: "mail-questions.php", //Change
+          type: "POST",
+          data: $('#questions').serialize(), //Change
+          success: function(response) {
+            alert("Спасибо за информацию!"); //Change
+            $.magnificPopup.close();
+            $('input[type="text"]').val('');
+          },
+          error: function(response) {
+            alert("Ошибка при отправке сообщения"); //Change
+         }
+        });
+    });
+    
+    $('#request').submit(function(event){ //Change
+        event.preventDefault();
+        $.ajax({
+          url: "mail-request.php", //Change
+          type: "POST",
+          data: $('#request').serialize(), //Change
+          success: function(response) {
+            alert("Спасибо за информацию!"); //Change
+            $.magnificPopup.close();
+            $('input[type="text"]').val('');
           },
           error: function(response) {
             alert("Ошибка при отправке сообщения"); //Change
@@ -17,14 +104,14 @@ $(function() {
     });
 
 	//Chrome Smooth Scroll
-	try {
-		$.browserSelector();
-		if($("html").hasClass("chrome")) {
-			$.smoothScroll();
-		}
-	} catch(err) {
-
-	};
+//	try {
+//		$.browserSelector();
+//		if($("html").hasClass("chrome")) {
+//			$.smoothScroll();
+//		}
+//	} catch(err) {
+//
+//	};
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
 	
